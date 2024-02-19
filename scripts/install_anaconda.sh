81b7c993bb0ba71ebf735a8208438acb6bc2a314
MG-RX93 <mgrx.93v@gmail.com>
2024-02-18 17:25:26 -0800

    Install Anaconda script

---
+++ scripts/install_anaconda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This helps to install anaconda using homebrew on Mac
+
+
+# Run Updates
+brew update --auto-update
+
+# Install Anaconda
+brew install anaconda
+
+# Will be prompted for password
+
+# The following will be outputed.
+# ==> Changing ownership of paths required by anaconda with sudo; the password may be necessary.
